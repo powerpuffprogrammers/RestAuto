3972a1bb53b68e14f25d695116eb97402a16e58d
Christina Segerholm <Christina Segerholm>
2016-02-25 11:33:44 -0500

    CS: Changed DatabaseA to DatabaseAController

--- RestApp/src/Restaurant.java
+++ RestApp/src/Restaurant.java
@@ -2,14 +2,12 @@
 
 import java.util.ArrayList;
 
-import dataBaseC.Party;
 
 //create shared memory that any process can access
 public class Restaurant {
 	
 	private String Name;
 	private short OwnerID;
-	private ArrayList<Party> WaitList;
 	
 	
 	public Restaurant(){
